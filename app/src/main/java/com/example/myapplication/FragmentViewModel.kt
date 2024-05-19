66668b9ff895992646f251df4f8a3a275435d436
IT22074072 <it22074072@my.sliit.lk>
2024-05-19 15:10:37 +0530

    Inter Fragment Communication. ViewModel class

---
+++ app/src/main/java/com/example/myapplication/FragmentViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FragmentViewModel : ViewModel() {
+    private val backgroundColor = MutableLiveData<Int>()
+
+    fun getBackgroundColor():LiveData<Int>{
+        return backgroundColor
+    }
+
+    fun setBackgroundColor(color: Int){
+        backgroundColor.value = color
+    }
+}
