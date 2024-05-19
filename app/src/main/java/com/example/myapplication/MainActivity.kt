95696a33a86eb8951c216f27768ff9036b02ce03
IT22074072 <it22074072@my.sliit.lk>
2024-05-19 12:49:11 +0530

    Loading the fragment on to the FragmentContainer in activity_main.xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Create Fragment objects in the MainActivity
+    private val homeFragment = HomeFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,46 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        //Initialize the button view and implement the onClickListeners
+        val button:Button = findViewById(R.id.button)
+        val button2:Button = findViewById(R.id.button2)
+
+        button.setOnClickListener(){
+            loadHome()
+
+        }
+
+        button2.setOnClickListener(){
+            loadSettings()
+
+        }
+
+    }
+
+    //implement the loadHome function
+    private fun loadHome(){
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+
+        if(fragment == null){
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, homeFragment).commit()
+        } else{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit()
+        }
+
+    }
+
+
+    //implement the loadSettings function
+    private fun loadSettings(){
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+
+        if(fragment == null){
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, settingsFragment).commit()
+        } else{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, settingsFragment).commit()
         }
     }
 }
